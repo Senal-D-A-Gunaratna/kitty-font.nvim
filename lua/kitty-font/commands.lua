068109d5ce38489f612527fce2ca8b63667b8288
Senal-D-A-Gunaratna <230862861+Senal-D-A-Gunaratna@users.noreply.github.com>
2026-07-27 22:06:45 +0530

    commands: add PaddingSet/PaddingIncrease/PaddingDecrease

--- lua/kitty-font/commands.lua
+++ lua/kitty-font/commands.lua
@@ -23,6 +23,29 @@ function M.setup(api)
     desc = "Pick font interactively",
     force = true,
   })
+
+  vim.api.nvim_create_user_command("PaddingSet", function(cmdopts)
+    local value = #cmdopts.fargs > 0 and table.concat(cmdopts.fargs, " ") or nil
+    api.set_padding(value)
+  end, {
+    nargs = "*",
+    desc = "Set Kitty window padding",
+    force = true,
+  })
+
+  vim.api.nvim_create_user_command("PaddingIncrease", function()
+    api.padding_increase()
+  end, {
+    desc = "Increase Kitty window padding",
+    force = true,
+  })
+
+  vim.api.nvim_create_user_command("PaddingDecrease", function()
+    api.padding_decrease()
+  end, {
+    desc = "Decrease Kitty window padding",
+    force = true,
+  })
 end
 
 return M
